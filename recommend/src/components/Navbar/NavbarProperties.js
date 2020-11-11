7e6df0075647cc88c6db8dd0a5a8a1788f0b1abe
Carlito Algarin <carlito1113@yahoo.com>
2020-11-11 16:09:26 -0500

    installed react router, added more navbar styling, and inserted a placeholder comment for import react scroll

--- recommend/src/components/Navbar/NavbarProperties.js
+++ recommend/src/components/Navbar/NavbarProperties.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import { Link as LinkRouter } from 'react-router-dom';
+// import { Link as LinkScroll } from 'react-scroll';
+
 
 export const Nav = styled.nav`
     background: #12232E;
@@ -11,4 +14,22 @@ export const Nav = styled.nav`
     position: sticky;
     top: 0;
     z-index: 10;
+
+    @media screen and (max-width: 960px) {
+        transition: 0.8s all ease;
+    }
+`
+
+export const NavbarContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    z-index: 1;
+    width: 100%;
+    padding: 0 24px;
+    max-width: 1100px;
+`
+
+export const NavLogo = styled(LinkRouter)`
+
 `
