d711e32e799d1e90f30ac47a4800a59710ca8e11
Carlito Algarin <carlito1113@yahoo.com>
2020-11-11 19:24:26 -0500

    added in sidebar(still needs functionality tweaks)

--- recommend/src/components/Sidebar/SidebarProperties.js
+++ recommend/src/components/Sidebar/SidebarProperties.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import {Link as LinkScroll} from 'react-scroll';
+import {Link as LinkRouter} from 'react-router-dom';
 import {FaTimes} from 'react-icons/fa';
 
 export const SidebarContainer = styled.aside`
@@ -27,4 +29,62 @@ export const Icon = styled.div`
     font-size: 2rem;
     cursor: pointer;
     outline: none;
+`;
+
+export const SidebarWrapper = styled.div`
+    color: #007CC7;
+`;
+
+export const SidebarMenu = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(6, 80px);
+    text-align: center;
+
+    @media screen and (max-width: 480px) {
+        grid-template-rows: repeat(6, 60px);
+    }
+`;
+
+export const SidebarLink = styled(LinkScroll)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    text-decoration: none;
+    list-style: none;
+    transition: 0.2s ease-in-out;
+    text-decoration: none;
+    color: #007CC7;
+    cursor: pointer;
+
+    &:hover {
+        color: #EEFBFB;
+        transition: 0.2s ease-in-out;
+    }
+`;
+
+export const SideBtnWrap = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+export const SidebarRoute = styled(LinkRouter)`
+    border-radius: 50px;
+    background: #EEFBFB;
+    white-space: nowrap;
+    padding: 16px 64px;
+    color: #12232E;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: #007CC7;
+        color: #12232E;
+    }
 `;
