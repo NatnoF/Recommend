3f0179dc72524d19ee78ca1cdbbc842942ad5d6f
Carlito Algarin <carlito1113@yahoo.com>
2020-11-12 15:11:10 -0500

    passed props information into the card

--- client/src/components/Card/Cards.js
+++ client/src/components/Card/Cards.js
@@ -2,14 +2,19 @@ import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
 
-function Cards() {
+function Cards(props) {
     return (
         <div className='cards'>
             <h1>These are some of our top picks!</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem 
+                        src=""
+                        text="placeholder book text"
+                        label="book genre"
+                        path="/services"
+                        />
                     </ul>
                 </div>
             </div>
