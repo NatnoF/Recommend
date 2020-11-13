eec7778eca9845048c0e6bab68fda68533669e11
Carlito Algarin <carlito1113@yahoo.com>
2020-11-12 21:45:11 -0500

    added in a copy of the signin page to sign up and adjusted text for its purpose

---
+++ client/src/components/SignUp/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+    Container,
+    FormWrap,
+    Icon,
+    FormContent,
+    Form,
+    FormH1,
+    FormLabel,
+    FormInput,
+    FormButton,
+    Text
+} from './SignUpProperties';
+
+
+const SignUp = () => {
+    return (
+        <>
+            <Container>
+                <FormWrap>
+                    <Icon to="/">Recommend</Icon>
+                    <FormContent>
+                        <Form action="#">
+                            <FormH1>Create your account</FormH1>
+                            <FormLabel htmlFor="for">Your Email</FormLabel>
+                            <FormInput type="email" required />
+                            <FormLabel htmlFor="for">Your Password</FormLabel>
+                            <FormInput type="password" required />
+                            <FormButton type="submit">Finish</FormButton>
+                        </Form>
+                    </FormContent>
+                </FormWrap>
+            </Container>
+        </>
+    );
+}
+
+export default SignUp;
