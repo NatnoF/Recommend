b67bbdafe7ae6d4d479d0fd045ae5ac349df5d71
Carlito Algarin <carlito1113@yahoo.com>
2020-11-15 01:53:01 -0500

    Changed some color styling in the sidebarmenu and added in cards to the feed to test scroll functionality

--- client/src/components/BookResults/index.js
+++ client/src/components/BookResults/index.js
@@ -1,39 +1,104 @@
-import React from 'react';
-import './BookResults.css';
-import CardItem from '../Card/CardItem';
-import '../Card/Cards.css';
+import React from "react";
+import "./BookResults.css";
+import CardItem from "../Card/CardItem";
+import "../Card/Cards.css";
 
 function BookResults() {
-    return (
-        <div className="bookresults">
-                <div className="cards">
-                    <div className="cards__container">
-                        {/* <div className="cards__wrapper"> */}
+  return (
+    <div className="bookresults">
+      <div className="cards">
+        <div className="cards__container">
+          {/* <div className="cards__wrapper"> */}
+          <ul className="cards__items">
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+            <CardItem
+              src="images/library.jpeg"
+              text="placeholder book text"
+              label="book genre"
+              path="/services"
+            />
+          </ul>
 
-
-                            <ul className="cards__items">
-                                                    <CardItem 
-                                                    src="images/library.jpeg"
-                                                    text="placeholder book text"
-                                                    label="book genre"
-                                                    path="/services"
-                                                    />
-                                                    <CardItem 
-                                                    src="images/library.jpeg"
-                                                    text="placeholder book text"
-                                                    label="book genre"
-                                                    path="/services"
-                                                    />
-                                                </ul>
-
-
-
-                        {/* </div> */}
-                    </div>
-                </div>
-                
+          {/* </div> */}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default BookResults;
