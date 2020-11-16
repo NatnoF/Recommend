b1f1a29853fdc15722594401de5ceea3978cca3a
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-16 15:44:46 -0500

    Added update function for bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -16,5 +16,10 @@ module.exports = {
     db.Book.create(req.body)
     .then(dbBook => res.json(dbBook))
     .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
   }
 };
