fb8abd8d353dc143645ca4f4d7547440760d90ef
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-16 15:44:21 -0500

    Added create function for bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -11,5 +11,10 @@ module.exports = {
     db.Book.findById(req.params.id)
     .then(dbBook => res.json(dbBook))
     .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Book.create(req.body)
+    .then(dbBook => res.json(dbBook))
+    .catch(err => res.status(422).json(err));
   }
 };
