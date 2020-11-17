b57d2b97cab0ad8d118f5754bc95bb28affaf627
Carlito Algarin <carlito1113@yahoo.com>
2020-11-17 11:14:09 -0500

    linked router to the sidebarmenu buttons and adjusted css to override the link styling to the original styling

--- client/src/components/SidebarMenu/SidebarOption.js
+++ client/src/components/SidebarMenu/SidebarOption.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './SidebarOption.css';
+import {Link} from 'react-router-dom';
 
-
-function SidebarOption({ active, text, Icon }) {
+function SidebarOption({ active, text, Icon, to }) {
     return (
-    <div className={`sidebarOption ${active && 'sidebarOption--active'}`}>
+    <Link to={ to } className={`sidebarOption ${active && 'sidebarOption--active'}`}>
             <Icon />
             <h2>{text}</h2>
-        </div>
+        </Link>
     );
 }
 
