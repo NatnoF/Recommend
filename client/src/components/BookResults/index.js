5150d869f04101384efbbbf8f9e46f3555e4a67a
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-17 16:38:16 -0500

    Finished the Search Results page, but not the links.

--- client/src/components/BookResults/index.js
+++ client/src/components/BookResults/index.js
@@ -3,109 +3,29 @@ import React from "react";
 import CardItem from "../Card/CardItem";
 import "../Card/Cards.css";
 import Grid from "@material-ui/core/Grid";
+import "./style.css";
 
-function BookResults() {
+function BookResults({ books }) {
   return (
       <div className="cards">
         <div className="cards__container">
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <CardItem
-                src="images/library.jpeg"
-                text="placeholder book text"
-                label="book genre"
-                path="/services"
-              />
-            </Grid>
-          </Grid>
+            {books.length ? (
+                <Grid container spacing={3}>
+                  {books.map(book => (
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                      <CardItem 
+                        key={book.id}
+                        id={book.id}
+                        title={book.volumeInfo.title}
+                        image={book.volumeInfo.imageLinks.thumbnail}
+                        authors={book.volumeInfo.authors.join(", ")}
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+              ) : (
+                <h2 className="text">{"No Results Found"}</h2>
+              )}
         </div>
       </div>
   );
