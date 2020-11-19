23579ddb54024067de1ba18b9c47a11cf24eda20
Carlito Algarin <carlito1113@yahoo.com>
2020-11-19 17:25:04 -0500

    replaced .div for link and title to a and p tags and added target _blank to have the gtihub links open in new tabs

--- client/src/components/Footer/footercontainer.js
+++ client/src/components/Footer/footercontainer.js
@@ -2,9 +2,25 @@ import React from "react";
 import Footer from "../Footer";
 
 export function FooterContainer() {
-    return(
-        <Footer>
-            
-        </Footer>
-    )
+  return (
+    <Footer>
+      <Footer.Wrapper>
+        <Footer.Row>
+          <Footer.Column>
+            <Footer.Title>Created By</Footer.Title>
+          </Footer.Column>
+          <Footer.Column>
+            <Footer.Title>
+              Carlito Algarin
+            </Footer.Title>
+            <Footer.Link href="https://github.com/Carlito1113" target="_blank">Github</Footer.Link>
+          </Footer.Column>
+          <Footer.Column>
+            <Footer.Title>Nicolas Fontan</Footer.Title>
+            <Footer.Link href="https://github.com/NatnoF" target="_blank">Github</Footer.Link>
+          </Footer.Column>
+        </Footer.Row>
+      </Footer.Wrapper>
+    </Footer>
+  );
 }
--- client/src/components/Footer/styles/footer.js
+++ client/src/components/Footer/styles/footer.js
@@ -2,19 +2,53 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     padding: 80px 60px;
-    background: radial-gradient(
+    background: rgba(18, 35, 46, 1);
+    /* radial-gradient(
         circle,
         rgba(32, 54, 71, 1) 0%,
-        rgba(18, 35, 46, 1) 100%)
-        
+        rgba(18, 35, 46, 1) 100%); */
 `
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        max-width: 1000px;
+        margin: 0 auto;
+`
 
-export const Column = styled.div``
+export const Column = styled.div`
+        display: flex;
+        flex-direction: column;
+        text-align: left;
+        margin-left: 60px;
+`
 
-export const Row = styled.div``
+export const Row = styled.div`
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+        grid-gap: 20px;
 
-export const Link = styled.div``
+        @media (max-width: 1000px) {
+            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        }
+`
 
-export const Title = styled.div``
+export const Link = styled.a`
+        color: #EEFBFB;
+        margin-bottom: 20px;
+        font-size: 18px;
+        text-decoration: none;
+
+        &:hover {
+            color: #007CC7;
+            transition: 200ms ease-in;
+        }
+`
+
+export const Title = styled.p`
+        font-size: 24px;
+        color: #EEFBFB;
+        margin-bottom: 40px;
+        font-weight: bold;
+`
