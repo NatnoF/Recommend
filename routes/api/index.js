d9812063f84c214da0cf09407268f8c8f5323cec
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-18 22:55:09 -0500

    Required comment routes in the api index

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const bookRoutes = require("./books");
 const googleRoutes = require("./google");
 const reviewRoutes = require("./reviews");
+const commentRoutes = require("./comments");
 
 // Book Routes
 router.use("/books", bookRoutes);
@@ -11,5 +12,8 @@ router.use("/google", googleRoutes);
 
 // Review routes
 router.use("/reviews", reviewRoutes);
+
+// Comment routes
+router.use("/comments", commentRoutes);
 
 module.exports = router;
