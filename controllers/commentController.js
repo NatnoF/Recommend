9b7c573d5514ab3de3ab486cb834af231af6718c
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-18 22:37:19 -0500

    Made the remove function for the commentController

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -15,5 +15,11 @@ module.exports = {
     db.Comment.findOneAndUpdate({ id: req.params.id }, req.body)
       .then(dbComment => res.json(dbComment))
       .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Comment.findById(req.params.id)
+      .then(dbComment => dbComment.remove())
+      .then(dbComment => res.json(dbComment))
+      .catch(err => res.status(422).json(err));
   }
 }
