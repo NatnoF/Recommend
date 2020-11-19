06af98fd2966394cacb509fb817aeae9c45f23b9
Carlito Algarin <carlito1113@yahoo.com>
2020-11-19 17:48:43 -0500

    added in github link to project in footer and changed sizing of text

--- client/src/components/Footer/footercontainer.js
+++ client/src/components/Footer/footercontainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from "../Footer";
+import "../Footer/styles/footer.css";
 
 export function FooterContainer() {
   return (
@@ -7,17 +8,21 @@ export function FooterContainer() {
       <Footer.Wrapper>
         <Footer.Row>
           <Footer.Column>
-            <Footer.Title>Created By</Footer.Title>
+            <Footer.Title><h1><strong>Creators</strong></h1></Footer.Title>
           </Footer.Column>
           <Footer.Column>
             <Footer.Title>
-              Carlito Algarin
+              <h5>Carlito Algarin</h5>
             </Footer.Title>
-            <Footer.Link href="https://github.com/Carlito1113" target="_blank">Github</Footer.Link>
+            <Footer.Link href="https://github.com/Carlito1113" target="_blank"><i class="fab fa-github"></i></Footer.Link>
           </Footer.Column>
           <Footer.Column>
-            <Footer.Title>Nicolas Fontan</Footer.Title>
-            <Footer.Link href="https://github.com/NatnoF" target="_blank">Github</Footer.Link>
+            <Footer.Title><h5>Nicolas Fontan</h5></Footer.Title>
+            <Footer.Link href="https://github.com/NatnoF" target="_blank"><i class="fab fa-github"></i></Footer.Link>
+          </Footer.Column>
+          <Footer.Column>
+            <Footer.Title><h5>Application Github</h5></Footer.Title>
+            <Footer.Link href="https://github.com/NatnoF/Recommend" target="_blank"><i class="fab fa-github"></i></Footer.Link>
           </Footer.Column>
         </Footer.Row>
       </Footer.Wrapper>
