2d5a286d5a0a91ba48f1ff28393e92773f8e30d4
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-19 11:03:56 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +10,9 @@ const app = express();
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Other middleware
+app.use(passport.initialize());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -15,6 +20,12 @@ if (process.env.NODE_ENV === "production") {
 
 // Add routes, both API and view
 app.use(routes);
+
+/* === Server-Side Authentication w/passport.js on our Model === */
+const Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // Connect to Mongo DB
 mongoose.connect(
