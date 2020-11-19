4b2d6302ef574bc78f885ff29316b37901cd437f
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-18 22:36:17 -0500

    Made the update function for the commentController

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -10,5 +10,10 @@ module.exports = {
     db.Comment.create(req.body)
       .then(dbComment => res.json(dbComment))
       .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Comment.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbComment => res.json(dbComment))
+      .catch(err => res.status(422).json(err));
   }
 }
