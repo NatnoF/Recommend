aff99f90b674ab65b7d468bca1d6a08b9537f30b
Carlito Algarin <carlito1113@yahoo.com>
2020-11-19 22:13:51 -0500

    added in a material-ui radio group for the recommend and not recommend selections

--- client/src/pages/book.js
+++ client/src/pages/book.js
@@ -6,6 +6,13 @@ import { Icon } from "../components/SidebarMenu/SidebarMenuProperties";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Card from "@material-ui/core/Card";
+
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
 const useStyles = makeStyles({
   root: {
@@ -21,6 +28,11 @@ const useStyles = makeStyles({
 const Book = () => {
   const [book, setBook] = useState({});
   const [count, setCount] = useState(0);
+  const [value, setValue] = React.useState("recommend");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
   const getBookInfo = () => {
     API.getBook(window.location.pathname.split("/").pop()).then((res) => {
@@ -114,24 +126,49 @@ const Book = () => {
                 </Grid>
               </Grid>
             </div>
-            
+
             <div className="wrapper">
-            <Grid className="flex-wrap-reverse" container spacing={12}> 
-                    <Grid item sm={12} justify="center">
-                      <Paper className="homeLink">
-                        <Icon to="/search">Reviews</Icon>
-                      </Paper>
-                    </Grid>
-            </Grid>
+              <Grid className="flex-wrap-reverse" container spacing={12}>
+                <Grid item sm={12} justify="center">
+                  <Paper className="homeLink">
+                    <Icon to="/search">Reviews</Icon>
+                  </Paper>
+                </Grid>
+              </Grid>
+
+              <Grid container spacing={3}>
+                <Grid item sm={4}>
+                  <Card>
+                  <FormControl component="fieldset">
+                    <RadioGroup
+                      aria-label="recommend"
+                      name="recommend1"
+                      value={value}
+                      onChange={handleChange}
+                    >
+                      <FormControlLabel
+                        value="true"
+                        control={<Radio />}
+                        label="Recommend"
+                      />
+                      <FormControlLabel
+                        value="false"
+                        control={<Radio />}
+                        label="Not Recommend"
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                  </Card>
+                </Grid>
+              </Grid>
             </div>
           </Grid>
-          
         </>
       ) : (
         <h1>Loading...</h1>
       )}
     </div>
   );
-}
+};
 
 export default Book;
