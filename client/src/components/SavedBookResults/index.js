6289eacc591038ae8e9bbacd0c8ab0d9d9dc0140
Carlito Algarin <carlito1113@yahoo.com>
2020-11-22 14:08:13 -0500

    added a background to the saved books div

--- client/src/components/SavedBookResults/index.js
+++ client/src/components/SavedBookResults/index.js
@@ -6,13 +6,13 @@ import "./style.css";
 
 function BookResults({ books }) {
   return (
-      <div className="cards">
+      <div className="cards background">
         <div className="cards__container">
             {books.length ? (
                 <Grid container spacing={3}>
                   {books.map(book => (
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                      <CardItem 
+                      <CardItem
                         key={book.googleId}
                         id={book.googleId}
                         title={book.title}
